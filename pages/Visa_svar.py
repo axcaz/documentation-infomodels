90e7f2a74319ca661cdaac64c7335c58ee5e5c08
axcaz <164012825+axcaz@users.noreply.github.com>
2025-04-02 11:58:09 +0200

    bugg fixad

--- pages/Visa_svar.py
+++ pages/Visa_svar.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.set_page_config(page_title="📊 Visa insamlade svar", layout="centered")
+st.set_page_config(page_title="Visa insamlade svar  📊", page_icon="📊", layout="centered")
 
 # Filnamn för data
 csv_file = "responses.csv"  # Fil med insamlade svar
